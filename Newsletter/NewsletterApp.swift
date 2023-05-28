9b6cda5548013b919d32d0e6a33a24bad781412f
Dams920 <50359853+Dams920@users.noreply.github.com>
2023-05-28 18:32:23 +0200

    Initial Commit

---
+++ Newsletter/NewsletterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsletterApp.swift
+//  Newsletter
+//
+//  Created by Damien Chailloleau on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsletterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
